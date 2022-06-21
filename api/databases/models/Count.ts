d1313766e9cb3af8e122c28da88991724c8da808
Alhena <alhenaplandsman@gmail.com>
2022-06-21 15:27:59 -0300

    Testing routes and creating models

--- api/databases/models/Count.ts
+++ api/databases/models/Count.ts
@@ -3,10 +3,13 @@ const CountSchema = new Schema({
     _id: Schema.Types.ObjectId,
     countName: {type: String, require: true},
     expenses: {
-        category: String,
+        category: {type: String, require: true},
         amount: Number
     },
-    entrance: {}
+    entrance: {
+        category: {type: String, require: true},
+        amount: Number
+    }
 },
 {
     timestamps: true  // fecha de creación y de actualización
