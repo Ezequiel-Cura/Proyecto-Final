52087d7a8a0ca13b6f02dcfc9b912839e63c672a
Alhena <alhenaplandsman@gmail.com>
2022-06-21 21:55:41 -0300

    adding typescript to models and fixing import and export

---
+++ api/databases/models/Account.js
@@ -0,0 +1,32 @@
+'use strict';
+const AccountSchema = new Schema({
+    _id: Schema.Types.ObjectId,
+    AccountName: { type: String, required: true },
+    expenses: [{
+            category: { type: String, required: true },
+            amount: Number
+        }],
+    entrance: [{
+            category: { type: String, required: true },
+            amount: Number
+        }]
+}, {
+    timestamps: true // fecha de creación y de actualización
+});
+// timestamps crea dos props: createdAt y la otra UpdateAt
+// routas get y necesita de req.body el año o mes que precisa
+// filter(account => account.createdAt.split("-")[0] === year)
+// filter(account => account.createdAt.split("-")[1] === month)
+// cuenta1{
+//     id,
+//     name: cuentaFija,
+//     gastos{
+// categoria: psicologo,
+// amount: 2000  
+//     }
+//     ingresos:{
+// categoria: herencia,
+// amount: 100000
+// }
+// }
+module.exports = AccountSchema;
