8352cfa75131043916c01e4d7d53c01bac9a2051
Alhena <alhenaplandsman@gmail.com>
2022-06-22 15:46:52 -0300

    "modularizando routes je..."

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,33 +1,13 @@
-import { Router, Request, Response } from "express";
-import db from "../../databases/models/index"; // importas todos los modelos como un objeto al que despues accedes con db.[modelo]
+import { Router } from "express";
+
  const router = Router()
 
+ import routesUser from './routesUser';
 
-router.post("/", async (req: Request, res: Response) => {
-    try {
-        const newUser: typeof Object = req.body;
-        const savingNewUser = await new db.User(newUser).save();
-        // console.log("result: ", result)
-        savingNewUser
-            ? res.status(200).json(`Successfully created a new user with id ${savingNewUser._id}`)
-            : res.status(500).send("Failed to create a new user.");
-    } catch (error: any) {
-        // console.error(error.message);
-        res.status(400).send("Error en la ruta post de user");
-    }
-});
+ router.use("/", routesUser)
 
 
 
-router.get("/", async (_req: Request, res: Response) => {
-    try {
-       const Users = await db.User.find({});
-       console.log("Users: ", Users) 
-       res.status(200).send(Users);
-    } catch (error: any) {
-        res.status(500).send(error.message);
-    }
-});
 
 
 export default router
---
+++ api/src/routes/routesUser.ts
@@ -0,0 +1,31 @@
+
+import { Router, Request, Response } from "express";
+import db from "../../databases/models/index"; // importas todos los modelos como un objeto al que despues accedes con db.[modelo]
+
+const router = Router()
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const newUser: object = req.body;
+        const savingNewUser = await db.User.create(newUser);
+
+        savingNewUser
+            ? res.status(200).json(`Successfully created a new user with id ${savingNewUser._id}`)
+            : res.status(500).send("Failed to create a new user.");
+    } catch (error: any) {
+
+        res.status(400).send("Error en la ruta post de user");
+    }
+});
+
+router.get("/", async (_req: Request, res: Response) => {
+    try {
+       const Users = await db.User.find({});
+       console.log("Users: ", Users) 
+       res.status(200).send(Users);
+    } catch (error: any) {
+        res.status(500).send(error.message);
+    }
+});
+
+export default router
