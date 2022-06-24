98ee6d160f8938b3d2b902d737400a79531f0ea6
Exivac <exivacz@gmail.com>
2022-06-24 09:44:32 -0300

    user delete by id

--- api/src/routes/routesUser.ts
+++ api/src/routes/routesUser.ts
@@ -1,12 +1,47 @@
 
 import { Router, Request, Response } from "express";
 import { ObjectId } from "mongodb";
-import db from "../../databases/models/index"; // importas todos los modelos como un objeto al que despues accedes con db.[modelo]
+import dotenv from 'dotenv'
+dotenv.config()
 import UserNoSqlTemp from "../../databases/models/UserNoSql(temp)";
 import bcrypt from 'bcrypt'
 
-
 const router = Router()
+
+interface value {
+  date?: string
+}
+
+const entriesUpdate = (key: string, value: object) => {
+  /*
+  monthlyInput
+  extraInput
+  monthlyExpenses
+  variableExpenses
+  */
+
+}
+
+router.get("/user", async (_req: Request, res: Response) => {
+  const { email, password } = _req.body
+
+  try {
+    const User = await UserNoSqlTemp.findOne({ email })
+    const passwordCompare = User && await bcrypt.compare(password, User.password)
+
+
+    if (!User) {
+      res.send('Usuario inexistente')
+    }
+    if (passwordCompare) {
+      res.send(User)
+    } else {
+      res.status(400).send('Contraseña Incorrecta')
+    }
+  } catch (error) {
+    res.send(error)
+  }
+});
 
 router.post("/user", async (req: Request, res: Response) => {
 
@@ -32,79 +67,44 @@ router.post("/user", async (req: Request, res: Response) => {
         password: hashPass
       })
     })
-    .then(() => {
-      res.status(200).send()
+    .then((user) => {
+      res.status(200).send(user)
     })
-    .catch(() => {
+    .catch((err) => {
+      console.log(err)
       res.status(400).send('Error en creacion de usuario')
     })
 
-
-
-
-
 });
 
-router.put("/user/:id", async (req: Request, res: Response) => {
-  const id = req?.params?.id;
+router.delete("/user", async (req: Request, res: Response) => {
+  const {id} = req.query
 
-  try {
-    const userToUpdate: typeof UserNoSqlTemp = req.body;
-    const query = { _id: new ObjectId(id) };
-
-    const updateUser = await db.UserNoSqlTemp.updateOne(query, { $set: userToUpdate });
-
-    updateUser
-      ? res.status(200).send(`Successfully updated user with id ${id}`)
-      : res.status(304).send(`User with id: ${id} not updated`);
-  } catch (error: any) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
-});
-
-router.get("/user", async (_req: Request, res: Response) => {
-  const {email, password} = _req.body
-
-  try {    
-    const User = await UserNoSqlTemp.findOne({email})
-    const passwordCompare = User && await bcrypt.compare(password, User.password)
-
-
-    if(!User){
-      res.send('Usuario inexistente')
-    }
-    if(passwordCompare){
-      res.send(User)
+  UserNoSqlTemp.findByIdAndDelete(id)
+  .then((user) => {
+    console.log(user)
+    if(user){
+      res.send('Usuario eliminado')
     } else {
-      res.status(400).send('Contraseña Incorrecta')
+      res.send('Usuario no encontrado')
     }
-  } catch (error) {
-    res.send(error)
-  }
+  })
+  .catch(() => {
+    res.send('Error en protocolo de borrado')
+  })
 });
 
-router.delete("/user/:id", async (req: Request, res: Response) => {
-  const id = req?.params?.id;
+router.put("/user", async (req: Request, res: Response) => {
+  const {id} = req.query
+  const {key, value} = req.body
 
-  try {
-    // console.log("id: ", id)
-    const query = { _id: new ObjectId(id) };
-    // console.log("query: ", query)
-    const deleteUser = await db.UserNoSqlTemp.deleteOne(query);
-    // console.log({deleteUser})
-    // deleteUser ahora tiene un obj con dos props: acknowledged: boolean y deletedCount: number
-    if (deleteUser && deleteUser.deletedCount) {
-      res.status(202).send(`Successfully removed user with id ${id}`);
-    } else if (!deleteUser) {
-      res.status(400).send(`Failed to remove user with id ${id}`);
-    } else if (!deleteUser.deletedCount) {
-      res.status(404).send(`User with id ${id} does not exist`);
-    }
-  } catch (error: any) {
-    console.error(error.message);
-    res.status(400).send(error.message);
-  }
+  
+
+
 });
+
+
+
+
 
 export default router
