f513bc87e871ffa70ca4f99526ba5b9dd3090d1e
Rocio Arroyo <rocionahir.arroyo@gmail.com>
2022-06-23 21:54:44 -0300

    subida componente

--- client/src/redux/reducers/userReducer.ts
+++ client/src/redux/reducers/userReducer.ts
@@ -1,82 +1,75 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from 'axios';
 
 interface User {
   // Mock OBJ
+  todosLosUsuarios: []
   usuario: object
   status: 'idle' | 'loading' | 'success' | 'failed'
 }
 
-
-
 const initialState: User = {
-  usuario: {
-    // id: '9vd8jq9834h91278gh1ufhv',
-    // nombre: 'Nombre',
-    // apellido: 'Apellido',
-    // email: 'email@email.com',
-    // contraseña: '24958v19er84y9w8345g412895g1',
-    // avatar: 'https://png.pngtree.com/png-vector/20210604/ourlarge/pngtree-gray-avatar-placeholder-png-image_3416697.jpg',
-
-    // cuenta:[
-    //   {
-    //     accountName: 'Account1',
-    //     expenses: [
-    //       {category: 'Alimento',
-    //         ammount: 2000},
-            
-    //       {category: 'Luz',
-    //         ammount: 3000}
-    //     ],
-    //     entrance: [
-    //       {category: 'Herencia',
-    //         ammount: 10000},
-
-    //       {category: 'Seguro',
-    //         ammount: 150000}
-    //     ]
-    //   }
-    // ]
-  },  
+  todosLosUsuarios: [],
+  usuario: {},  
   status: 'idle'
 }
+
+export const getAllUsers : any = createAsyncThunk('user/getAllUsers', 
+  (obj, hola) => {
+    console.log("segundo argumento: ", hola)
+  return axios
+    .get("http://localhost:3001")
+    .then(response => response.data)
+    .catch(error => error)
+})
 
 const reducerSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      console.log(action.payload, 'REDUCERRRR')
+      state.usuario = action.payload
+    },
     loadMockUser: (state) => {
       state.usuario = {
-        id: '9vd8jq9834h91278gh1ufhv',
-        nombre: 'Nombre',
-        apellido: 'Apellido',
-        email: 'email@email.com',
-        contraseña: '24958v19er84y9w8345g412895g1',
-        avatar: 'https://png.pngtree.com/png-vector/20210604/ourlarge/pngtree-gray-avatar-placeholder-png-image_3416697.jpg',
-    
-        cuenta:[
-          {
-            accountName: 'Account1',
-            expenses: [
-              {category: 'Alimento',
-                ammount: 2000},
-
-              {category: 'Luz',
-                ammount: 3000}
-            ],
-            entrance: [
-              {category: 'Herencia',
-                ammount: 10000},
-                
-              {category: 'Seguro',
-                ammount: 150000}
-            ]
-          }
-        ]
+        "Account": {
+          "monthlyInput": [
+            {
+              "date": "2022-06-22T23:58:46.550Z",
+              "description": "Ta caro el gym",
+              "amount": 5000,
+              "_id": "62b3acbe003d297af5e75ead"
+            }
+          ],
+          "extraInput": [],
+          "monthlyExpenses": [],
+          "variableExpenses": []
+        },
+        "_id": "62b3acbe003d297af5e75eac",
+        "userName": "Bon Jovi",
+        "lastName": "Jovi",
+        "email": "bon@jovitojejox",
+        "avatar": "jovitoPic",
+        "__v": 0
       }
       
     }
+  },
+  extraReducers: {
+    [getAllUsers.pending]: (state) => {
+      state.status = "loading"
+    },
+    [getAllUsers.fulfilled]: (state, {payload}) => {
+      state.status = "success"
+      state.todosLosUsuarios = payload
+    },
+    [getAllUsers.rejected]: (state, {payload}) => {
+      state.status = "failed"
+      console.log("error: ", payload)
+    },
   }
 })
 
-export const {loadMockUser} = reducerSlice.actions
+export const {loadMockUser, setUser} = reducerSlice.actions
 export default reducerSlice.reducer
