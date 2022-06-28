9bb7877dd0c983de073e04212c0c2a72eb6106a7
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-06-28 16:52:50 -0300

    funciona el reduce

--- client/src/components/Detalles/Detalles.jsx
+++ client/src/components/Detalles/Detalles.jsx
@@ -1,16 +1,45 @@
 import React from "react";
 import Nav from "components/Nav/Nav";
 import styles from "./Detalles.module.css";
+import { useAppSelector } from "redux/hooks";
 
 export default function Detalles() {
-  let border1 = "2px solid green";
+  const usuario = useAppSelector((state) => state.user.usuario);
+  console.log(usuario);
+  let border1 = "2px solid white";
 
   const styleBar = {
     border: border1,
-    "border-radius": "10px",
+    borderRadius: "10px",
     overflow: "hidden",
-    display: "grid",
-    "grid-template-columns": "1fr 1fr 1fr 1fr 1fr",
+    display: "flex",
+  };
+
+  function calculate() {
+    const ingresos = usuario.Account.extraInput.reduce(
+      (prev, actual) => {
+        return prev + actual.amount
+      },
+      0
+    );
+    const gastos = usuario.Account;
+    return ingresos;
+  }
+  console.log(calculate());
+  function random() {
+    return Math.floor(Math.random() * 100);
+  }
+
+  const incomes = {
+    background: "green",
+    width: random() + "%",
+    height: "100px",
+  };
+
+  const gastos = {
+    background: "red",
+    width: random() + 100 + "%",
+    height: "100px",
   };
 
   return (
@@ -18,12 +47,10 @@ export default function Detalles() {
       <Nav />
       <div className={styles.detalles_wrapper}>
         <h1>Detalles</h1>
+
         <div style={styleBar}>
-          <div className={styles.apples}>20</div>
-          <div className={styles.bananas}>5</div>
-          <div className={styles.peaches}>10</div>
-          <div className={styles.cherries}>2</div>
-          <div className={styles.grapes}>12</div>
+          <div style={incomes}>{} </div>
+          <div style={gastos}>{} </div>
         </div>
       </div>
     </div>
