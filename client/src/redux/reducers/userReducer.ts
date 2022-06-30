2ac0af64757a2e08b81f0f10f81a71c7fecdcd5e
Alhena <alhenaplandsman@gmail.com>
2022-06-30 20:43:23 -0300

    reducer

--- client/src/redux/reducers/userReducer.ts
+++ client/src/redux/reducers/userReducer.ts
@@ -115,7 +115,9 @@ export const deleteDato: any = createAsyncThunk("user/deleteIngreso",
 export const addCategory: any = createAsyncThunk("user/addCategory",
   async (ingreso, { rejectWithValue }) => {
     try {
+      console.log({ingreso})
       const { data } = await axios.post(`/user/category`, ingreso)
+      console.log(data, "DATAAAA")
       return data
     } catch (err: any) {
       return rejectWithValue(err.response.data)
