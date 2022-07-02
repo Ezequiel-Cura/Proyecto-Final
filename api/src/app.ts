7577fe6cc13e758c400440ba1d59a96dcdbdcc57
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-07-02 18:51:49 -0300

    testing

--- api/src/app.ts
+++ api/src/app.ts
@@ -17,7 +17,7 @@ server.use(cookieparser());
 server.use(morgan("dev"));
 server.use(cors(
   {
-    origin: "https://proyecto-final-lime-beta.vercel.app", 
+    origin: ['https://proyecto-final-lime-beta.vercel.app'], 
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: ['Accept', 'Content-Type'],
     credentials: true
