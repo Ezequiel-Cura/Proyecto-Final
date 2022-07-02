e3591a9dde4151083d8da0ff68de3a83ef87a14a
Alhena <alhenaplandsman@gmail.com>
2022-07-02 13:02:49 -0300

    saves

--- client/src/components/Saves/SavesCreate.tsx
+++ client/src/components/Saves/SavesCreate.tsx
@@ -4,41 +4,75 @@ import { addSaving, deleteSaving } from 'redux/reducers/userReducer';
 
 export default function SavesCreate() {
 
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
+  const { usuario } = useAppSelector(state => state.user);
 
-    interface SavingUser{
-        name: string,
-        start: string,
-        end: string,
-        goal?: number,
-        place: string,
-        currency: string,
-        amount: number
-      }
+  interface SavingUser {
+    name: string,
+    start: string,
+    end: string,
+    goal?: number,
+    place: string,
+    currency: string,
+    amount: number
+  }
 
-    const [input, setInput] = useState<SavingUser>({
-        name: '',
-        start: '',
-        end: '',
-        goal: 0,
-        place: '',
-        currency: '',
-        amount: 0
-      });
+  const [input, setInput] = useState<SavingUser>({
+    name: '',
+    start: '',
+    end: '',
+    goal: 0,
+    place: '',
+    currency: '',
+    amount: 0
+  });
 
-      function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setInput({
-          ...input,
-          [e.target.name]: e.target.value
-        })
-      }
-    function createSaving(){
-        dispatch(addSaving())
-      }
-      return(
-        <div>
-            <h1>HOLIS</h1>
-            {/* <button onClick={}>X</button> */}
-        </div>
-      )
+  const form = {
+    id: usuario._id,
+    value: input
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {         //-----Form
+    e.preventDefault();
+    dispatch(addSaving());
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+      <input
+                type='text'
+                name='name'
+                value={input.name}
+                placeholder='Agrega un nombre'
+                onChange={handleChange}
+              >
+              </input>
+              <input
+                type='date'
+                name='end'
+                value={input.end}
+                placeholder='Agrega una fecha'
+                onChange={handleChange}
+              >
+              </input>
+              <label>$</label>
+              <input
+                type='number'
+                name='goal'
+                min='0'
+                value={input.goal}
+                placeholder='Monto'
+                onChange={handleChange}
+              >
+              </input>
+              <button type='submit'>Agregar</button>
+      </form>
+    </div>
+  )
 }
