c2821854d3ec1dea66fc87e79e20a52e24eb55dd
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-07-02 13:32:55 -0300

    primeros cambios al home

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import styles from "./Home.module.css"
 import Nav from "components/Nav/Nav"
 import { Link } from 'react-router-dom'
+import { useAppSelector } from "redux/hooks"
 
 export default function Home() {
-
+  const usuario = useAppSelector((state)=> state.user.usuario)
+  console.log(usuario)
   return (
     <div className={styles.home_wrapper}>
       <Nav/>
@@ -18,6 +20,7 @@ export default function Home() {
               <Link to="/home/ingresos">
                 <div className={styles.ingresoLink}>
                     <h2>INGRESOS</h2> 
+                    <span>Monto actual: ${} </span>
                 </div>
               </Link>
           </div>
@@ -26,6 +29,7 @@ export default function Home() {
             <Link to="/home/gastos">
               <div className={styles.gastos_link}>
                   <h2>GASTOS</h2>
+                  <span>Monto actual: ${} </span>
               </div>
             </Link>
           </div>
@@ -35,6 +39,7 @@ export default function Home() {
             <Link to="/home">
               <div className={styles.ahorros_link}>
                   <h2>AHORROS</h2>
+                  <span>Monto actual: ${} </span>
               </div>
             </Link>
           </div>
@@ -43,6 +48,14 @@ export default function Home() {
               <Link to="/home/detalles">
                 <div className={styles.detalles_link}>
                     <h2>DETALLES</h2>            
+                </div>
+              </Link>
+          </div>
+
+          <div className={styles.expand_columns}>
+              <Link to="/home">
+                <div className={styles.novedades_link}>
+                    <h2>NOVEDADES</h2>            
                 </div>
               </Link>
           </div>
