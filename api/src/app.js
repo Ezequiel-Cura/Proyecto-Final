714ecba291e27c9fa59738dc48cd29b89e13393d
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-07-02 23:09:08 -0300

    saque el cors

--- api/src/app.js
+++ api/src/app.js
@@ -9,7 +9,6 @@ const routes_1 = __importDefault(require("./routes"));
 const morgan_1 = __importDefault(require("morgan"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const cors_1 = __importDefault(require("cors"));
 // Initializations
 (0, db_1.connectDB)();
 const server = (0, express_1.default)();
@@ -18,7 +17,6 @@ server.use(body_parser_1.default.urlencoded({ extended: true }));
 server.use(body_parser_1.default.json());
 server.use((0, cookie_parser_1.default)());
 server.use((0, morgan_1.default)("dev"));
-server.use((0, cors_1.default)());
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.FRONT_URL || "http://localhost:3000/"); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
--- api/src/app.ts
+++ api/src/app.ts
@@ -16,7 +16,6 @@ server.use(bodyparser.json());
 server.use(cookieparser());
 server.use(morgan("dev"));
 
-server.use(cors())
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.FRONT_URL || "http://localhost:3000/"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
