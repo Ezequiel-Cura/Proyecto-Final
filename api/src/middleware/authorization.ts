1b31b3281ae32d3508ecf45aa4a03fb753d2c5bd
Aitor <98376581+Aitortita@users.noreply.github.com>
2022-07-03 15:49:37 -0300

    ?

--- api/src/middleware/authorization.ts
+++ api/src/middleware/authorization.ts
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken"
 
 export default function authorization(req: any, res: any, next: any){
+  try {
     const token = req.cookies.access_token
-    if (!token) return res.sendStatus(403).send("no access token");
-    try {
+    if (!token) return res.sendStatus(401).end()
       const data:any = jwt.verify(token, process.env.JWTPRIVATEKEY);
       req.userId = data._id;
       next();
     } catch (err: any) {
-      res.sendStatus(403).send(err.message);
+      return res.sendStatus(401).send(err.message);
     }
 }
