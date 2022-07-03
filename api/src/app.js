5c21e4507263d36145c0ce7dc1e3d26e99e8c739
Ezequiel-Cura <91684822+Ezequiel-Cura@users.noreply.github.com>
2022-07-02 23:02:01 -0300

    otra vez

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use((0, cookie_parser_1.default)());
 server.use((0, morgan_1.default)("dev"));
 server.use((0, cors_1.default)());
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', `${process.env.FRONT_URL}`); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', process.env.FRONT_URL || "http://localhost:3000/"); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
