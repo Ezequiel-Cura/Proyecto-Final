4edd75587424b37983674ca8b29372fafbf20940
Rocio Arroyo <rocionahir.arroyo@gmail.com>
2022-07-05 18:37:22 -0300

    link agregado en nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -57,63 +56,3 @@ function App() {
 }
 
 export default App;
-=======
-import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-
-// COMPONENTS
-import Home from 'components/Home/Home';
-import Landing from 'components/Landing/Landing';
-import Profile from 'components/Profile/Profile';
-import Input from 'components/Ingreso/Input';
-import InputTable from 'components/Ingreso/InputTable';
-import Expenses from 'components/Gastos/Expenses';
-import ExpensesTable from 'components/Gastos/ExpensesTable';
-import Detalles from 'components/Detalles/Detalles';
-import { useAppDispatch, useAppSelector } from 'redux/hooks';
-import { getUserInfo } from 'redux/modules/getUserInfo';
-import { ProtectedRoute } from 'utils/ProtectedRoutes';
-import LostPage from 'components/LostPage/LostPage';
-import ControlPanel from 'components/Admin/ControlPanel/ControlPanel';
-import Novedades from 'components/Novedades/Novedades';
-import SavesLanding from 'components/Saves/SavesLanding';
-import Saves from 'components/Saves/Saves';
-
-
-
-function App() {
-  const dispatch = useAppDispatch()
-  const { usuario } = useAppSelector((({user}) => user))
-  const [logged, setLogged] = useState(true)
-
-  useEffect(()=> {
-    if(localStorage.getItem("logged")) dispatch(getUserInfo())
-    else setLogged(false)
-  },[])
-
-  return (
-    <Routes>
-      <Route path="/" element={<Landing/>}/>
-      <Route element={<ProtectedRoute isAllowed={logged}/>}>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/home/ingresos' element={<Input/>}/>
-        <Route path='/home/ingresos/add' element={<InputTable/>}/>
-        <Route path='/home/gastos' element={<Expenses/>}/>
-        <Route path='/home/gastos/add' element={<ExpensesTable/>}/>
-        <Route path='/home/saving' element={<SavesLanding/>}/>
-      <Route path='/home/saving/add' element={<Saves/>}/>
-        <Route path='/home/detalles' element={<Detalles/>}/>
-        <Route path="/home/novedades" element={<Novedades/>} />
-      </Route>
-      <Route path='/admin' element={<ProtectedRoute isAllowed={logged && usuario.role === "admin"}/>}>
-        <Route path="/admin/controlPanel" element={<ControlPanel/>}/>
-      </Route>
-      
-      <Route path="*" element={<LostPage/>}/>
-    </Routes>
-  );
-}
-
-export default App;
->>>>>>> 23b81426e3e0e438c204afaec4a6eec0617b216b
--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from 'redux/hooks'
@@ -59,65 +58,3 @@ const {usuario} = useAppSelector(({user}) => user)
     </div>
   )
 }
-=======
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { useAppDispatch, useAppSelector } from 'redux/hooks'
-import styles from "./Nav.module.css"
-import imagePlaceholder from "assets/imagePlaceholder.jpg"
-import { logout } from 'redux/modules/logout'
-
-
-export default function Nav() {
-const navigate = useNavigate()
-const dispatch = useAppDispatch()
-const {usuario} = useAppSelector(({user}) => user)
-  return (
-    <div className={styles.Nav_wrapper}>
-        <div className={styles.image_wrapper}>
-            <Link to="/profile">
-                <img src={usuario.avatar ? usuario.avatar : imagePlaceholder} className={styles.image_container}alt="foto de perfil"/>
-                {usuario ? <span>{usuario.firstName} </span> : null}
-            </Link>
-        </div>
-        <div className={styles.items_wrapper}>
-            <Link to="/home">
-                <div className={styles.Nav_items_wrapper}>
-                    <h4>Home</h4>
-                </div>
-            </Link>
-            <Link to="/home/ingresos">
-                <div>
-                    <h4>Ingresos</h4>
-                </div>
-            </Link>
-            <Link to="/home/gastos">    
-                <div>
-                    <h4>Gastos</h4>
-                </div>
-            </Link>
-            <Link to="/home/saving">
-                <div>
-                    <h4>Ahorros</h4>
-                </div>
-            </Link>
-            <Link to="/home/detalles">
-                <div>
-                    <h4>Detalles</h4>
-                </div>
-            </Link>
-            {usuario.role === "admin" &&
-            <Link to="/admin/controlPanel">
-                <div>   
-                    <h4>Panel de Control</h4>
-                </div>
-            </Link>
-            }
-        </div>
-            <button className={styles.logout} onClick={()=> dispatch(logout()).then(()=>navigate("/", {state: {registered: true}})).then(()=>window.location.reload())}>
-                Salir
-            </button>
-    </div>
-  )
-}
->>>>>>> 23b81426e3e0e438c204afaec4a6eec0617b216b
