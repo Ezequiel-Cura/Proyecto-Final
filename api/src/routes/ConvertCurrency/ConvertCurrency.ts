38a586da2d523a771d3cc18c4535e4a850f761c8
Alhena <alhenaplandsman@gmail.com>
2022-07-07 16:41:11 -0300

    currency init

--- api/src/routes/ConvertCurrency/ConvertCurrency.ts
+++ api/src/routes/ConvertCurrency/ConvertCurrency.ts
@@ -1,22 +1,28 @@
-// var myHeaders = new Headers();
+let myHeaders = new Headers();
 // myHeaders.append("apiKeyConvert", "t9Vou5wgBj8SIH0BAa3bzgAdpbnUBGXY");
 
 import { Router, Request, Response } from "express"
 
-// var requestOptions = {
-//   method: 'GET',
-//   redirect: 'follow',
-//   headers: myHeaders
-// };
+var requestOptions: RequestInit = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
 const { apiKeyConvert } = process.env
 
 const router = Router()
 
 router.get("/",  async (req: Request, res: Response) => {
-//   fetch("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+    const { to, from, amount } = req.query
+    try {
+        const apiData = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`, requestOptions)
+
+        res.status(200).send(apiData)
+    } catch (error) {
+        console.log(error)
+        res.status(404).send(error)
+    }
+
 })
 
 export default router;
