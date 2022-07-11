cc701f58f36153dfe57a5806c237b6e097e15f66
Alhena <alhenaplandsman@gmail.com>
2022-07-11 20:56:15 -0300

    fix route of currency

--- api/src/routes/ConvertCurrency/ConvertCurrency.ts
+++ api/src/routes/ConvertCurrency/ConvertCurrency.ts
@@ -16,7 +16,7 @@ var requestOptions: RequestInit = {
 };
 
 router.get("/", async (req: Request, res: Response) => {
-    const { to, from, amount } = req.body
+    const { to, from, amount } = req.query
     console.log({ to, from, amount })
     try {
         if (!to || !from || !amount) {
--- client/src/redux/reducers/userReducer/actions/getCurrency.ts
+++ client/src/redux/reducers/userReducer/actions/getCurrency.ts
@@ -4,8 +4,9 @@ import axios from "axios";
 
 export const getCurrency: any = createAsyncThunk("currency",
 async (ingreso: any, { rejectWithValue }) => {
+  const { to, from, amount } = ingreso
   try {
-    let getCurrencyData: any = await axios.get("/currency", {
+    let getCurrencyData: any = await axios.get("/currency?to="+to+"&from="+from+"&amount="+amount, {
       data: ingreso
     });
     return getCurrencyData.data
