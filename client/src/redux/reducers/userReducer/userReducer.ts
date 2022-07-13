140ca7c8c5a76cbe0a7c8fcdc0ba49bebff50170
Exivac <exivacz@gmail.com>
2022-07-12 21:18:11 -0300

    Category create form refreshes after creation as means of confirmation

--- client/src/redux/reducers/userReducer/userReducer.ts
+++ client/src/redux/reducers/userReducer/userReducer.ts
@@ -75,7 +75,7 @@ interface User {
   //     rating: number
   //   }
   // }
-  status: 'idle' | 'loading' | 'success' | 'failed'
+  status: 'idle' | 'loading' | 'success' | 'failed' | any
   allInputs: Entries[] | [],
   allOutputs: Entries[] | [],
   renderInputs: Entries[] | [],
@@ -417,7 +417,7 @@ const reducerSlice = createSlice({
       state.status = "loading"
     },
     [addCategory.fulfilled]: (state, { payload }) => {
-      state.status = "success"
+      state.status = "CategoryCreated"
       state.usuario = payload
     },
     [addCategory.rejected]: (state) => {
