fdb2612f693c9d8980c265d3771dc7b0d451deb5
Exivac <exivacz@gmail.com>
2022-07-13 21:48:37 -0300

    LPM

--- client/src/components/Premium/ProductDisplay.tsx
+++ client/src/components/Premium/ProductDisplay.tsx
@@ -2,23 +2,22 @@ import Nav from 'components/Nav/Nav';
 import React from 'react';
 import axios from 'axios'
 
-
-
+const logo = require('../../assets/favicon.ico')
 export const ProductDisplay = () => (
     <section style={{ display: "grid", gridTemplateColumns: "178px 1fr" }}>
         <Nav/>
         <div className="product">
             <img
-                src="https://i.imgur.com/EHyR2nP.png"
+                src={logo}
                 alt="The cover of Stubborn Attachments"
             />
             <div className="description">
-                <h3>Stubborn Attachments</h3>
-                <h5>$20.00</h5>
+                <h3>Premium</h3>
+                <h5>$500</h5>
             </div>
-        </div>
         <form action='http://localhost:3001/user/premium/buy' method='POST'>
-            <button type="submit">Checkout</button>
+            <button type="submit">Consigue premium</button>
         </form>
+        </div>
     </section>
 );
